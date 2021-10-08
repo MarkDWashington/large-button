d4d043d9ffd06a224a717ca5fd842511c20c167a
Mark Washington <markdw88@gmail.com>
2021-10-08 11:33:16 -0400

    Initial commit

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# large-button
+A button that, when you click it, counts the number of times it has been clicked.
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="TheQuestion">Clear?</div>
+      <button className="TheButton">Clear!</button>
+    </div>
+  );
+}
+
+export default App;
