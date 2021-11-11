6b1f83840b741fb97c823cc65f0a7752b6587395
Mark Washington <markdw88@gmail.com>
2021-11-10 20:59:25 -0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App(props) {
+  const [count, setCount] = useState("Loading...");
+  
+  useEffect(() => {
+    axios.get("http://localhost:3001/current")
+    .then((res) => {
+      if (res.status === 200) {
+        setCount(res.data);
+      } else {
+        console.log(res.status);
+      }
+    })
+    .catch(console.err);
+  });
+  
+  var increment = async () => {
+    axios.get("http://localhost:3001/increment")
+    .then((res) => {
+      if (res.status === 200) {
+        setCount(res.data);
+        console.log(count);
+      } else {
+        console.log("Failed: ", res.status);
+      }
+    })
+    .catch(console.error);
+  };
+
   return (
     <div className="App">
-      <div className="TheQuestion">Clear? {this.state.testvar}</div>
-      <button className="TheButton" onClick={this.testFunc}>Clear!</button>
+      <div className="TheQuestion">Clear?</div>
+      <button className="TheButton" onClick={async () => increment()}>Clear!</button>
+      <div className="TheCount">({count})</div>
     </div>
   );
 }
-
-// class App extends React.Component {
-
-//   constructor() {
-//     super();
-//     this.state = {testvar: "none"};
-//   }
-
-//   componentDidMount() {
-//     console.log("Component Mounted");
-//   }
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="TheQuestion">Clear? {this.state.testvar}</div>
-//         <button className="TheButton" onClick={this.testFunc}>Clear!</button>
-//       </div>
-//     );
-//   }
-
-//   testFunc = async () => {
-//     console.log("Test function called");
-//   }
-
-// }
 
 export default App;
